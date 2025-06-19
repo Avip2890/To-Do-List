acc91f13727e1b93ee86bb4442dec32724c86d13
Avip2890 <Avrahamp@one1.co.il>
2025-06-19 12:24:10 +0300

    Delete BackEnd/src directory

--- BackEnd/src/Api/ExportTasks.py
+++
@@ -1,9 +0,0 @@
-﻿from flask import Blueprint, jsonify
-from BackEnd.src.todo.logic import export_tasks
-
-export_tasks_bp = Blueprint('export_tasks', __name__)
-
-@export_tasks_bp.route('/tasks/export', methods=['GET'])
-def export_tasks_route():
-    result = export_tasks()
-    return jsonify({'message': 'Tasks exported successfully', 'details': result})
--- BackEnd/src/Api/ListTasks.py
+++
@@ -1,9 +0,0 @@
-﻿from flask import Blueprint, jsonify
-from BackEnd.src.todo.logic import load_tasks
-
-list_tasks_bp = Blueprint('list_tasks', __name__)
-@list_tasks_bp.route('/tasks', methods=['GET'])
-def list_tasks_route():
-    tasks = load_tasks()
-    return jsonify(tasks), 200
-
